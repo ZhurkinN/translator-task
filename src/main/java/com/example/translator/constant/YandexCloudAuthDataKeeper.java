3ebefe670ca801a8c443bff6e80bae49b3792e72
ZhurkinN <zhurkin236@gmail.com>
2022-09-13 13:47:31 +0300

    fix: token

--- src/main/java/com/example/translator/constant/YandexCloudAuthDataKeeper.java
+++ src/main/java/com/example/translator/constant/YandexCloudAuthDataKeeper.java
@@ -2,7 +2,7 @@ package com.example.translator.constant;
 
 public class YandexCloudAuthDataKeeper {
 
-    public static final String IAM_TOKEN = "t1.9euelZqYyMeTx4uPzZfKlorGmJ6axu3rnpWay56SypGamJqNxsjNno_KzMjl9PdkVwFn-e9UATWI3fT3JAZ_ZvnvVAE1iA.9pvZtZwXJVfZdlCrzKoNikblE1NSxP04-M9Cb9r2Yt_Ioshd67D2r6LEe2CdyeXNPPUoxn22sJ1s8v4xfYWfDg";
+    public static final String IAM_TOKEN = "t1.9euelZrHjsfMmI2WxpiWi5fIm5mLzu3rnpWay56SypGamJqNxsjNno_KzMjl8_dzQH5m-e9ffh1U_t3z9zNve2b5719-HVT-.ZI2bhksUiqOSUQG-XA-HuzHUaH95HL-T5cedvLyGzLDgApCrfOPZtjGfdt5zlZczxL3vPOVRKh3q2gD8ER1TDw";
     public static final String FOLDER_ID = "b1gj00pqbbnce054l7cr";
 
     public static final long DURATION_TIME = 30L;
